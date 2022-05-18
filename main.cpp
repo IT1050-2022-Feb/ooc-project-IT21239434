60a6d4ceef261c7a046b84ae7e6877e71184f372
Kalpa punchihewa <99349489+IT21239434@users.noreply.github.com>
2022-05-18 06:37:40 +0000

    completed

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include"UnregisteredUser.h"
+#include"RegisteredUser.h"
+#include"Booking.h"
+#include"Taxi.h"
+#include"Offer.h"
+#include"Payment.h"
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	//object creation
+	UnregisteredUser Ureg1;
+	RegisteredUser reg1;
+	Booking book1;
+	Taxi* t1 = new Taxi;
+	Offer* o1 = new Offer;
+	Payment* p1 ;
+
+	//methods implementation
+	Ureg1.displayUnreUser();
+
+	reg1.registration();
+	reg1.login();
+
+	book1.addbookingdetails();
+	book1.cancelbookingdetails();
+
+	t1->addtaxiInformation();
+	t1->displaytaxi();
+
+	o1->addoffer();
+	o1->displayoffer();
+	o1->addpayment();
+
+	p1->checkpayment();
+	p1->displaypayment();
+};
